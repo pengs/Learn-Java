0946466280248ec1fda92336dcf4e3e35665ca1f
石鹏 <shipeng@uccc.cc>
2018-04-16 14:52:53 +0800

    添加学习示例

---
+++ src/art.java
@@ -0,0 +1,22 @@
+
+public class art {
+
+    public static  void main(String[] agrs){
+     int STB=89;
+     int Java=90;
+     int SQL=60;
+
+     //求Java课和sql课的分数之差
+        int score;
+        score=Java-SQL;
+        System.out.println(score);
+
+     //3门课程的平均分
+     float average_score;
+       average_score=(STB+Java+SQL)/3;
+       System.out.println(average_score);
+
+     }
+}
+
+
