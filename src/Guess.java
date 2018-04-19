681a599e9d80638085c8fbf4fd03f51418194c14
石鹏 <shipeng@uccc.cc>
2018-04-19 17:20:38 +0800

    no message

--- src/Guess.java
+++ src/Guess.java
@@ -4,7 +4,7 @@ public class Guess{
     public static void main(String[] args){
         System.out.println("---猜拳游戏---");
         System.out.println("请出拳：（1、剪刀 2、石头 3、布）");
-        Scanner in=new Scanner(System.in);
+        Scanner in=new Scanner(System.in);//Scanner类来获取用户的输入。
         int person=in.nextInt();//接收输入的数据
         int computer=(int)(Math.random()*3+1);//电脑随机输出，通过Math取随机数
         String Marks="";    //标记用户的手势
@@ -31,10 +31,9 @@ public class Guess{
                 Marks1="布";
                 break;
         }
-
         if (person<=0||person>3){
 
-            System.out.println("您输入的数据不正确哟；无法匹配，请重新输入");
+            System.out.println("您输入的数字不正确哟，无法匹配，请重新输入（1-3之间的整数）");
 
         }else if(person==computer){
 
